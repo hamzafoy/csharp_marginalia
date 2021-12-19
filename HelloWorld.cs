c4c56703acc5e119b53ec7bd8f25790ed8f72848
Ross Foy <hamzaoflouisville@gmail.com>
2021-12-19 18:42:59 -0500

    pushing boilerplate + Hello World print program.

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace FirstProgramStudyingCSharp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+//Study of some Basic C# Syntax from a Basic 'Hello World' Program
+
+/*
+
+
+
+*/
