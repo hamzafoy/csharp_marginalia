45a43f08ae5bc2f3ffb92d420b0538c5db5fb1fa
Ross Foy <hamzaoflouisville@gmail.com>
2021-12-25 18:27:41 -0500

    adding more notes.

--- weatherUtilities.cs
+++ weatherUtilities.cs
@@ -3,4 +3,12 @@
 - It is common to have 1 class per file and for the filename and the class name
 to be the same.
 
+- Namespaces help with name clashes between classes.
+
+- You can import and alias namespaces like so:
+`using WeatherUtilities = MyUtilities.WeatherUtilities;
+
+- `System` is the namespace that contains very fundamental methods, classes, including
+the access to the Console.
+
 */
