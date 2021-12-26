e4fea9ece3b5d69f1a74c09cd0018e73a758f469
Ross Foy <hamzaoflouisville@gmail.com>
2021-12-26 12:47:32 -0500

    adding more notes.

--- weatherUtilities.cs
+++ weatherUtilities.cs
@@ -7,6 +7,9 @@ to be the same.
 
 - You can import and alias namespaces like so:
 `using WeatherUtilities = MyUtilities.WeatherUtilities;
+If you do not opt to import namespaces, you'll need to use the fully
+qualified namespace to access a class in another file, like so:
+`MyUtilities.WeatherUtilities . . .`
 
 - `System` is the namespace that contains very fundamental methods, classes, including
 the access to the Console.
