66e860c95f8bd79eb7e15997e6a60f36e8ad1ffd
Ross Foy <hamzaoflouisville@gmail.com>
2022-01-12 19:14:26 -0500

    Begin documenting notes on the  type in C#.

---
+++ integerPlayground.cs
@@ -0,0 +1,7 @@
+//Integers are declared using `int`
+int a = 2;
+//The type for `b` is int because `a` stores an integer and is added to 3 which is also an integer.
+//There is no type conflict because the expression `a + 3` results in an integer.
+int b = a + 3;
+
+Console.WriteLine(b);
